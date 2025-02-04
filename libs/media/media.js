91166177828e49e8fa3d84628cf8f5cf09240962
Marnix <marnixkok+github@gmail.com>
2025-02-04 22:46:17 +1300

    (task) make media path base url configurable

--- libs/media/media.js
+++ libs/media/media.js
@@ -117,7 +117,7 @@ class MediaModal {
 		this.filemanager = null;
 		this.breadcrumbs = null;
 		this.fileList = null;
-		this.mediaPath = "/public/media/";
+		this.mediaPath = window.meta?.mediaBaseUrl ?? "/public/media/";
 		this.type = "single";
 	}
 	
